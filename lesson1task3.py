8857809bfc00f077ae405579b4fbd7c25483dca6
Бухарцев Владимир <collobot@gmail.com>
2021-11-03 15:37:02 +0300

    Выполнено 3 задание.

--- lesson1task3.py
+++ lesson1task3.py
@@ -1,2 +1,10 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python3
+
+for number in range(1, 101):
+    if number // 10 == 1 or number % 10 > 4 or number % 10 == 0:
+        print('{} процентов'.format(number))
+    elif number % 10 > 1 and number % 10 < 5:
+        print('{} процента'.format(number))
+    else:
+        print('{} процент'.format(number))
