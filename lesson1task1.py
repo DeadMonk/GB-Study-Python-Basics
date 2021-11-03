7746c3b8d43cc27ab9ae87269bb359ae955f2303
Бухарцев Владимир <collobot@gmail.com>
2021-11-03 11:09:19 +0300

    2 варианта 1-го задания

--- lesson1task1.py
+++ lesson1task1.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python3
 
-duration = input('Введите количество секунд: ')
-
-duration = int(duration)
+duration = int(input('Введите количество секунд: '))
 
 if duration >= 86400:
     print(duration // 86400, 'дн', (duration % 86400) // 3600, 'час', ((duration % 86400) % 3600) // 60, 'мин', duration % 60, 'сек')
@@ -14,7 +12,15 @@ elif duration >= 60:
 else:
     print(duration, 'сек')
 
-print(duration % 60, duration // 60 % 60, duration // 60 // 60, duration // 60 // 60 // 24)
-
-
-divisor = [86400, 3600, 60]
+divisor_list = [86400, 3600, 60, 1]
+unit_list = [ ' дн, ', ' час, ', ' мин, ', ' сек']
+result_list = []
+result_string = ''
+for divisor_id in range(len(divisor_list)):
+    result_list.append(duration // divisor_list[divisor_id])
+    duration = duration % divisor_list[divisor_id]
+for unit_id in range(len(unit_list)):
+    if result_list[unit_id] == 0:
+        continue
+    result_string = result_string + str(result_list[unit_id]) + unit_list[unit_id]
+print(result_string)
