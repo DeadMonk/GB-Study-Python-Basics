6b03cc18485431d692314e4fc527c3ba5809acb2
Бухарцев Владимир <collobot@gmail.com>
2021-11-03 14:54:05 +0300

    Убрано неудачное решение из задания 1, задание 2 выполнено.

--- lesson1task1.py
+++ lesson1task1.py
@@ -2,25 +2,14 @@
 #!/usr/bin/python3
 
 duration = int(input('Введите количество секунд: '))
-
-if duration >= 86400:
-    print(duration // 86400, 'дн', (duration % 86400) // 3600, 'час', ((duration % 86400) % 3600) // 60, 'мин', duration % 60, 'сек')
-elif duration >= 3600:
-    print(duration // 3600, 'час', (duration % 3600) // 60, 'мин', (duration % 3600) % 60, 'сек')
-elif duration >= 60:
-    print(duration // 60, 'мин', duration % 60, 'сек')
-else:
-    print(duration, 'сек')
-
+# Решение №1
 divisor_list = [86400, 3600, 60, 1]
-unit_list = [ ' дн, ', ' час, ', ' мин, ', ' сек']
-result_list = []
+unit_list = ['дн', 'час', 'мин', 'сек']
 result_string = ''
 for divisor_id in range(len(divisor_list)):
-    result_list.append(duration // divisor_list[divisor_id])
+    result = duration // divisor_list[divisor_id]
     duration = duration % divisor_list[divisor_id]
-for unit_id in range(len(unit_list)):
-    if result_list[unit_id] == 0:
+    if result == 0:
         continue
-    result_string = result_string + str(result_list[unit_id]) + unit_list[unit_id]
+    result_string += '{} {} '.format(result, unit_list[divisor_id])
 print(result_string)
