d6e5c45a3a81f77e9465dd5d5008b9a39aed3461
Бухарцев Владимир <collobot@gmail.com>
2021-11-05 01:29:15 +0300

    Вспомнил про enumerate

--- lesson1task1.py
+++ lesson1task1.py
@@ -5,9 +5,9 @@ duration = int(input('Введите количество секунд: '))
 divisor_list = [86400, 3600, 60, 1]
 unit_list = ['дн', 'час', 'мин', 'сек']
 result_string = ''
-for divisor_id in range(len(divisor_list)):
-    result = duration // divisor_list[divisor_id]
-    duration = duration % divisor_list[divisor_id]
+for divisor_id, divisor in enumerate(divisor_list):
+    result = duration // divisor
+    duration = duration % divisor
     if result == 0:
         continue
     result_string += '{} {} '.format(result, unit_list[divisor_id])
