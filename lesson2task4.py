f33e6b1c0dc50c8b49bdac84584dda46757aa944
Бухарцев Владимир <collobot@gmail.com>
2021-11-09 15:32:32 +0300

    Добавлено 2-е решение в 4 задание

--- lesson2task4.py
+++ lesson2task4.py
@@ -3,5 +3,13 @@
 
 base_list = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
 
+
 for employee in base_list:
     print(f'Привет, {employee.split()[-1].title()}!')
+
+
+for employee in base_list:
+    print(f'Привет, {employee[employee.rfind(" ") + 1:].title()}!')
+
+
+
