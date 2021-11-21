ff8510f69476124672a14c56fbd8609542cd28fc
Бухарцев Владимир <collobot@gmail.com>
2021-11-21 15:19:21 +0300

    Error

--- lesson6task6_7.py
+++ lesson6task6_7.py
@@ -2,3 +2,8 @@
 # !/usr/bin/python3
 
 
+def add_record(file_name: str, record):
+    with open(file_name, a, encoding='utf-8') as dfile:
+        dfile.write(record)
+
+add_record('test.csv', 5123.5)
