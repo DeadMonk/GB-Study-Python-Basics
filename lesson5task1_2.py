5207ab9f795b1bd860c49abcdf22adf26e3133a4
Бухарцев Владимир <collobot@gmail.com>
2021-11-21 11:49:38 +0300

    Правки

--- lesson5task1_2.py
+++ lesson5task1_2.py
@@ -2,12 +2,16 @@
 # !/usr/bin/python3
 
 
+# def yield_gen(count=1):
+#     for number in range(1, count+1):
+#         if number % 2:
+#             yield number
+
 def yield_gen(count=1):
-    for number in range(1, count+1):
-        if number % 2:
-            yield number
+    for number in range(1, count+1, 2):
+        yield number
 
-print(*(number for number in range(1, int(input('Y='))) if number % 2))
+non_yield_gen = (number for number in range(1, int(input('Y='))) if number % 2)
 
-print(*yield_gen(int(input('X='))))
+
 
