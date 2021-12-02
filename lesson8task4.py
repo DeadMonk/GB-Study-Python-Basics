2efaf3632b9c2d9263ca34619316be2d99b02c50
Бухарцев Владимир <collobot@gmail.com>
2021-12-02 14:34:23 +0300

    Check

--- lesson8task4.py
+++ lesson8task4.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 # !/usr/bin/python3
 
+from inspect import getsource
+
 def val_checker(callback):
     def _val_checker(func):
         def check_func(arg):
             if callback(arg):
                 return func(arg)
             else:
-                raise ValueError('Не соответствует условию.')
+                raise ValueError(f'Не соответствует условию. {getsource(callback)}')
         return check_func
     return _val_checker
 
@@ -15,3 +17,4 @@ def val_checker(callback):
 def calc_cube(x):
     return x ** 3
 
+print(calc_cube(-2))
